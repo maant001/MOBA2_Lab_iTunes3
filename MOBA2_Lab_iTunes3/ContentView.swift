eef70bf9018b9e0a93f75e77b138916bc6f8b386
maant001 <mamaril_a@me.com>
2023-05-13 16:01:06 +0200

    init
    - still weird bug
    - crashes when items loaded in single album view

--- MOBA2_Lab_iTunes3/ContentView.swift
+++ MOBA2_Lab_iTunes3/ContentView.swift
@@ -7,15 +7,73 @@
 
 import SwiftUI
 
+
+// views
+
 struct ContentView: View {
+    @State var artistList = [ArtistItem] ()
+    @State var searchEntry : String = "The Rolling Stones"
+    
     var body: some View {
+        
+        // TODO
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            HStack {
+                TextField("Search", text: $searchEntry, onCommit: {
+                    self.artistList = Jsonhelpers.loadAlbums(searchEntry: self.searchEntry)
+                })
+                Image(systemName: "magnifyingglass")
+            }.padding().background(Color(.secondarySystemBackground)).cornerRadius(15.5)
+                
+            NavigationView {
+                List(artistList) { artistItem in
+                    
+                    NavigationLink(destination: AlbumDetailView(album: Jsonhelpers.loadOneAlbum(collectionId: artistItem.id), songs: Jsonhelpers.loadSongs(collectionId: artistItem.id), collectionId: artistItem.id)) {
+                        
+                        HStack {
+                            Image(uiImage: artistItem.albumCover ?? UIImage()).shadow(radius: 3)
+                            VStack {
+                                Text(artistItem.collectionName!).frame(maxWidth: .infinity, alignment: .center)
+                                Text(artistItem.artistName!).font(.footnote)
+                            }
+                        }
+                        
+                    }
+
+                }.onAppear() {
+                    DispatchQueue.main.async {
+                        self.artistList = Jsonhelpers.loadAlbums(searchEntry: self.searchEntry)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct AlbumDetailView : View {
+    @State var album : ArtistItem
+    @State var songs = [Song] ()
+    var collectionId : Int
+    
+    var body : some View {
+        
+        VStack(alignment: .center) {
+            
+            Text(album.collectionName!)
+            Text(album.formattedReleaseDate!).font(.subheadline)
+            Image(uiImage: album.albumCover ?? UIImage())
+            
+            List(songs) { song in
+                
+                HStack {
+                    
+                    //Text(String(song.trackNumber ?? 0))
+                    //Text(song.trackName!)
+                    //Text(String(song.time))
+                }
+            }
+        }
     }
 }
 
@@ -24,3 +82,90 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+// structs
+
+struct ArtistItem: Identifiable, Decodable {
+    var artistName : String?
+    var collectionName : String?
+    var collectionType : String?
+    var collectionId : Int?
+    var id: Int {
+        get {
+            return collectionId ?? 0
+        }
+    }
+    
+    // important that the attribute names are the same than in call/json!
+    var artworkUrl100: String?
+    var albumCover : UIImage? {
+        get {
+            return loadAlbumImage(urlImage: self.artworkUrl100)
+        }
+    }
+    
+    var releaseDate: String?
+    var formattedReleaseDate: String? {
+        get {
+            return String(releaseDate!.prefix(9))
+        }
+    }
+    
+    func loadAlbumImage(urlImage: String?) -> UIImage? {
+        if urlImage != nil {
+            let url = NSURL(string: urlImage!)! as URL
+            
+            if let imageContent: NSData = NSData(contentsOf: url) {
+                let albumImage = UIImage(data: imageContent as Data)
+                return albumImage
+            }
+        }
+        
+        return nil
+    }
+}
+
+struct ArtistWrapper: Decodable {
+    // needs to be named results, because check json file!
+    // "results": [...
+    var results : [ArtistItem]
+}
+
+struct Song: Identifiable, Decodable {
+    var trackId: Int?
+    var wrapperType: String?
+    var trackName: String?
+    //var trackNumber: Int?
+    var trackTimeMillis: Int?
+    var id: Int {
+        get {
+            return trackId ?? 0
+        }
+    }
+    
+    var time: String {
+        get {
+            return convertMilliSeconds(trackTimeMillis!)
+        }
+    }
+    
+    func convertMilliSeconds(_ timeInMilliSeconds : Int) -> String {
+        let seconds = timeInMilliSeconds / 1000
+        let minutes = seconds / 60
+        let true_seconds = seconds / 60
+        var string_seconds = String(true_seconds)
+        
+        if (string_seconds.count == 1) {
+            string_seconds = "0" + string_seconds
+        }
+        
+        return String(minutes) + ":" + string_seconds
+    }
+}
+
+struct SongWrapper: Decodable {
+    var results: [Song]
+}
+
+
---
+++ MOBA2_Lab_iTunes3/jsonhelpers.swift
@@ -0,0 +1,64 @@
+//
+//  jsonhelpers.swift
+//  MOBA2_Lab_iTunes3
+//
+//  Created by Tony Mamaril on 13.05.23.
+//
+
+import Foundation
+
+class Jsonhelpers {
+    // helpers
+
+    class func loadAlbums(searchEntry: String) -> [ArtistItem] {
+        do {
+            let urlString = "https://itunes.apple.com/search?term=" + searchEntry + "&entity=album"
+            let urlEncoded = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+            let urlFinal = URL(string: urlEncoded)
+            let data = try Data(contentsOf: urlFinal!)
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(ArtistWrapper.self, from: data)
+            
+            return decodedData.results.filter({
+                return $0.collectionType != nil
+            })
+        } catch {
+            fatalError("json not loaded\n\(error)")
+        }
+    }
+
+    class func loadSongs(collectionId: Int) -> [Song] {
+        do {
+            let urlString = "https://itunes.apple.com/lookup?id=" + String(collectionId) + "&entity=song"
+            let urlEncoded = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+            let urlFinal = URL(string: urlEncoded)
+            let data = try Data(contentsOf: urlFinal!)
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(SongWrapper.self, from: data)
+            
+            return decodedData.results.filter({
+                return $0.wrapperType != nil
+            })
+        } catch {
+            fatalError("json not loaded\n\(error)")
+        }
+    }
+
+    class func loadOneAlbum(collectionId: Int) -> ArtistItem {
+        do {
+            let urlString = "https://itunes.apple.com/lookup?id=" + String(collectionId) + "&entity=song"
+            let urlEncoded = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+            let urlFinal = URL(string: urlEncoded)
+            let data = try Data(contentsOf: urlFinal!)
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(ArtistWrapper.self, from: data)
+            
+            return decodedData.results.filter({
+                return $0.collectionType != nil
+            })[0]
+        } catch {
+            fatalError("json not loaded\n\(error)")
+        }
+    }
+    
+}
