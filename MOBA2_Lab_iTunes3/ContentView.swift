1912e58146ae8fd889c202ff54b0a4725dc2facf
maant001 <mamaril_a@me.com>
2023-05-13 14:21:24 +0200

    Initial Commit

---
+++ MOBA2_Lab_iTunes3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MOBA2_Lab_iTunes3
+//
+//  Created by Tony Mamaril on 13.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MOBA2_Lab_iTunes3/MOBA2_Lab_iTunes3App.swift
@@ -0,0 +1,17 @@
+//
+//  MOBA2_Lab_iTunes3App.swift
+//  MOBA2_Lab_iTunes3
+//
+//  Created by Tony Mamaril on 13.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MOBA2_Lab_iTunes3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
